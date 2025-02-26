7f43ede84303ad1268b909f6584883e64fe75aac
gokuldev <gokuldev.p123@gmail.com>
2025-02-27 00:32:08 +0530

    http headeer and status codes

---
+++ Session_4_Getting_Started_with_Express/4.http_header_and_status_code/express-server.js
@@ -0,0 +1,38 @@
+import express from 'express'; // Import the Express framework
+
+// Create an instance of the Express application
+const server = express();
+
+// Middleware function to handle default GET request at the root path
+server.get('/', (req, res, next) => {
+  console.log('First middleware hit');
+  next(); // Pass control to the next middleware or route handler
+});
+
+// Handle GET request at the root path
+server.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('The GET method requests a representation of the specified resource. Requests using GET should only retrieve data and should not contain a request content.');
+});
+
+// Handle POST request at the root path
+server.post('/', (req, res) => {
+  res.status(201).send('The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.');
+});
+
+// Handle PUT request at the root path
+server.put('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.status(200).send('The PUT method replaces all current representations of the target resource with the request content.');
+});
+
+// Handle DELETE request at the root path
+server.delete('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.status(202).send('The DELETE method deletes the specified resource.');
+});
+
+// Assign the server to listen on port 3200
+server.listen(3200, () => {
+  console.log(`Server running at http://localhost:3200`);
+});
