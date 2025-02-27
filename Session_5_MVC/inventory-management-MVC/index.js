b3958f7f68d5edbc6b5591405cf81e6417fa2657
gokuldev <gokuldev.p123@gmail.com>
2025-02-27 21:47:36 +0530

    Setting Up View enginee and ejs

--- Session_5_MVC/inventory-management-MVC/index.js
+++ Session_5_MVC/inventory-management-MVC/index.js
@@ -1,14 +1,19 @@
 // Importing the express module to create a web server.
 import express from 'express';
-
+import path from 'path';
 // Importing the ProductController class for handling product-related routes.
 import ProductController from './src/controllers/product.controller.js';
-
 // Initializing the express application.
 const server = express();
 
+// Setting 'ejs' as the view engine for rendering HTML templates.
+server.set("view engine", "ejs");
+
+// Setting the directory for the views (templates) to the 'src/views' folder.
+server.set("views", path.resolve("src", "views"));
+
 // Defining the port number. It uses an environment variable if available, otherwise defaults to 8000.
-const port = 8000 // || process.env.PORT ||
+const port = 8000 //||  process.env.PORT; // Use the process.env.PORT if available, otherwise default to 8000.
 
 // Creating an instance of the ProductController.
 const productController = new ProductController();
--- Session_5_MVC/inventory-management-MVC/src/controllers/product.controller.js
+++ Session_5_MVC/inventory-management-MVC/src/controllers/product.controller.js
@@ -1,12 +1,16 @@
-import path from 'path'; // Importing the path module for file path operations
+// Importing the ProductModel from the models directory
 import ProductModel from '../models/product.model.js'; // Importing the model in product controller
 
 // Defining the ProductController class
 export default class ProductController {
     // Method to handle GET requests for product data
     getProduct(req, res) {
-        let products = ProductModel.get() // getting prodduct details
+        // Retrieving product details using the get method of ProductModel
+        let products = ProductModel.get() // Getting product details
+        // Rendering the 'products' view (HTML template) and passing the products data to it
+        return res.render('products', { products: products });
+
         // Sending the products.html file as a response
-        return res.sendFile(path.resolve("src", 'views', "products.html"));
+        // return res.sendFile(path.resolve("src", 'views', "products.html"));
     }
 }
