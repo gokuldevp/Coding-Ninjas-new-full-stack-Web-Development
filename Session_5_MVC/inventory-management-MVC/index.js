25ec88fd2e1ff41e12db469a6dab635cdc8ea270
gokuldev <gokuldev.p123@gmail.com>
2025-02-27 22:02:46 +0530

    Creating layout

--- Session_5_MVC/inventory-management-MVC/index.js
+++ Session_5_MVC/inventory-management-MVC/index.js
@@ -1,8 +1,11 @@
 // Importing the express module to create a web server.
 import express from 'express';
 import path from 'path';
+import expressEjsLayouts from 'express-ejs-layouts';
+
 // Importing the ProductController class for handling product-related routes.
 import ProductController from './src/controllers/product.controller.js';
+
 // Initializing the express application.
 const server = express();
 
@@ -11,6 +14,7 @@ server.set("view engine", "ejs");
 
 // Setting the directory for the views (templates) to the 'src/views' folder.
 server.set("views", path.resolve("src", "views"));
+server.use(expressEjsLayouts);
 
 // Defining the port number. It uses an environment variable if available, otherwise defaults to 8000.
 const port = 8000 //||  process.env.PORT; // Use the process.env.PORT if available, otherwise default to 8000.
